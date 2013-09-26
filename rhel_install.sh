078c4ae43233a8afada7e6ba5c41937d088444e3
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2013-09-26 17:55:48 +0200

    Updated RHEL install script.

--- rhel_install.sh
+++ rhel_install.sh
@@ -9,26 +9,27 @@ fi
 INSTALL_DIR=`readlink -f $1`
 
 # First we get the needed yum packages
-yum groupinstall "Development Tools"
-yum install qt-devel git cmake libXmu-devel libXi-devel freeglut-devel boost-devel libxml2-devel apr-devel httpd-devel
+#yum groupinstall "Development Tools"
+#yum install qt-devel git cmake libXmu-devel libXi-devel freeglut-devel boost-devel libxml2-devel apr-devel httpd-devel
 
 mkdir install_tmp
 cd install_tmp
 
 # Download GLEW:
-wget -O glew.tgz http://downloads.sourceforge.net/project/glew/glew/1.10.0/glew-1.10.0.tgz?r=http%3A%2F%2Fglew.sourceforge.net%2F&ts=1380204337&use_mirror=kent
+wget  -O glew.tgz  'http://downloads.sourceforge.net/project/glew/glew/1.10.0/glew-1.10.0.tgz?r=http%3A%2F%2Fglew.sourceforge.net%2F&ts=1380204337&use_mirror=kent'
+
 tar xvf glew.tgz
 cd glew-1.10.0
 GLEW_DEST=${INSTALL_DIR} make install
 cd ..
 
 # Download GLM
-wget -O glm.zip http://downloads.sourceforge.net/project/ogl-math/glm-0.9.4.6/glm-0.9.4.6.zip?r=http%3A%2F%2Fglm.g-truc.net%2F0.9.4%2Findex.html&ts=1380204611&use_mirror=kent
+wget -O glm.zip 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.4.6/glm-0.9.4.6.zip?r=http%3A%2F%2Fglm.g-truc.net%2F0.9.4%2Findex.html&ts=1380204611&use_mirror=kent' 
 unzip glm.zip
 cp -r glm/glm ${INSTALL_DIR}/include
 
 # Download Tinia
-git clone http://github.com/sintefmath/tinia.git
+git clone https://github.com/sintefmath/tinia.git
 cd tinia
 git checkout kjetilly_rhel_fixing
 mkdir build
@@ -39,7 +40,7 @@ cd ..
 cd ..
 
 # Download HPMC
-git clone http://github.com/sintefmath/hpmc.git
+git clone https://github.com/sintefmath/hpmc.git
 cd hpmc
 git checkout kjetilly_rhel_maintenance_fix
 mkdir build
@@ -49,26 +50,25 @@ make -j9 install
 cd ..
 cd ..
 
-# Download FRView
-git clone http://github.com/sintefmath/FRView.git
-cd FRView
-git checkout kjetilly_rhel_fixing
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}
+# Compile frview
+mkdir frview_build
+cd frview_build
+
+cmake ../.. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}
 make -j9 install
-cd ..
 cd ..
 
 # Install Apache locally:
 cp -r /etc/httpd ${INSTALL_DIR}/
-rm -rf ${INSTALL_DIR}/httpd/log ${INSTALL_DIR}/httpd/run ${INSTALL_DIR}/httpd/modules
-mkdir ${INSTALL_DIR}/httpd/log ${INSTALL_DIR}/httpd/run
+rm -rf ${INSTALL_DIR}/httpd/logs ${INSTALL_DIR}/httpd/run ${INSTALL_DIR}/httpd/modules
+mkdir ${INSTALL_DIR}/httpd/logs ${INSTALL_DIR}/httpd/run
 ln -s /etc/httpd/modules ${INSTALL_DIR}/httpd/modules
-sed -i "s/ServerRoot \/etc\/httpd/ServerRoot ${INSTALL_DIR}/httpd/g" ${INSTALL_DIR}/httpd/conf/httpd.conf
+SED_SAFE_INSTALL_DIR=${INSTALL_DIR//\//\\\/}
+sed -i "s/ServerRoot \\\"\\/etc\\/httpd\\\"/ServerRoot \\\"${SED_SAFE_INSTALL_DIR}\\/httpd\\\"/g" ${INSTALL_DIR}/httpd/conf/httpd.conf
 sed -i 's/Listen 80/Listen 8080/g' ${INSTALL_DIR}/httpd/conf/httpd.conf
 
-TO_INSERT <<EOF
+cat >> ${INSTALL_DIR}/httpd/conf/httpd.conf <<EOF
+
 LoadModule trell_module ${INSTALL_DIR}/var/trell/module/libmod_trell.so
 TrellMasterId "trell_master"
 TrellMasterExe "${INSTALL_DIR}/var/trell/bin/trell_master"
@@ -97,11 +97,12 @@ Alias /trell/static ${INSTALL_DIR}/var/trell/static
 </Directory>
 EOF
 
-echo $TO_INSERT >> ${INSTALL_DIR}/httpd/conf/httpd.conf
 
-echo <<EOF 
-To run the httpd server, issue the command
-
-   LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${INSTALL_DIR}/lib:${INSTALL_DIR}/lib64 httpd -d ${INSTALL_DIR}/httpd
-
-EOF
+echo ""
+echo ""
+echo "Done installing"
+echo ""
+echo "To run the httpd server, issue the command"
+echo " "
+echo "   LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${INSTALL_DIR}/lib:${INSTALL_DIR}/lib64 httpd -d ${INSTALL_DIR}/httpd"
+echo ""
