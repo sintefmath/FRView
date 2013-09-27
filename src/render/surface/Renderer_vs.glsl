641d69262be976b7ebb2dcdfae3d086f4858472d
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2013-09-27 15:01:00 +0200

    Added updated license information.

--- src/render/surface/Renderer_vs.glsl
+++ src/render/surface/Renderer_vs.glsl
@@ -1,12 +1,22 @@
 #version 330
-/******************************************************************************
+/* Copyright STIFTELSEN SINTEF 2013
  *
- *  Author(s): Christopher Dyken <christopher.dyken@sintef.no>
+ * This file is part of FRView.
+ * FRView is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * FRView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  Copyright (C) 2009 by SINTEF.  All rights reserved.
- *
- ******************************************************************************/
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the FRView.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 layout(location=0) in vec4 position;
 
 out GI {
