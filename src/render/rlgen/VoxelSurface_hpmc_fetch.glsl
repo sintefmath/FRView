6c73b50ac0f31689d7b28591a1b3ccd20ac4f14d
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2013-09-28 13:44:22 +0200

    Removed non-xml friendly tokens from shader license comment.

--- src/render/rlgen/VoxelSurface_hpmc_fetch.glsl
+++ src/render/rlgen/VoxelSurface_hpmc_fetch.glsl
@@ -12,7 +12,7 @@
  * GNU Affero General Public License for more details.
  *  
  * You should have received a copy of the GNU Affero General Public License
- * along with the FRView.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the FRView.  If not, see http://www.gnu.org/licenses/.
  */
 
 #ifndef VOXEL_SAMPLER_DEFINED
--- src/render/screen/TransparencyWeightedAverage_vs.glsl
+++ src/render/screen/TransparencyWeightedAverage_vs.glsl
@@ -13,7 +13,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with the FRView.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the FRView.  If not, see http://www.gnu.org/licenses/.
  */
 
 layout(location=0) in vec4 position;
