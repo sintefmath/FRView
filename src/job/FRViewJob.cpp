b6ada2ca55940c149345a995614625246e73f997
Christopher Dyken <cdyk@users.noreply.github.com>
2014-02-27 13:33:40 +0100

    Disabled low pri nagging from GL logging.

--- src/job/FRViewJob.cpp
+++ src/job/FRViewJob.cpp
@@ -801,10 +801,8 @@ FRViewJob::initGL()
         LOGGER_DEBUG( log, "Enabling OpenGL debug logging" );
         glEnable( GL_DEBUG_OUTPUT );
         glDebugMessageCallback( debugLogger, NULL );
-        glDebugMessageControl( GL_DONT_CARE,
-                               GL_DONT_CARE,
-                               GL_DEBUG_SEVERITY_LOW,
-                               0, NULL, GL_TRUE );
+        glDebugMessageControl( GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE );
+        glDebugMessageControl( GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_LOW, 0, NULL, GL_FALSE );
 
     }
     m_has_context = true;
